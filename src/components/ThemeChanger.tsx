614fbc376f5d5453c9eb308895356861bbcbbcd9
Adrián Antón <adrtler@gmail.com>
2021-12-12 12:15:21 +0100

    Theme context

---
+++ src/components/ThemeChanger.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState, ReactElement } from 'react';
+import { ThemeContext } from '../context';
+interface Props {
+  children?: ReactElement | ReactElement[];
+}
+
+export const ThemeChanger = ({ children }: Props) => {
+  const { Provider } = ThemeContext;
+  const [themeState, setThemeState] = useState(false);
+
+  const handleChange = () => {
+    setThemeState(!themeState);
+    if (themeState) {
+      localStorage.setItem('Theme', 'dark');
+      document.body.classList.add('dark');
+    } else {
+      localStorage.setItem('Theme', 'light');
+      document.body.classList.remove('dark');
+    }
+  };
+  useEffect(() => {
+    const getTheme = localStorage.getItem('Theme');
+    if (getTheme === 'dark') return document.body.classList.add('dark');
+  });
+  return (
+    <Provider value={{ darkMode: themeState, changeTheme: handleChange }}>
+      {children}
+    </Provider>
+  );
+};
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import './navigation.scss';
+import { ThemeChanger } from '../components';
 interface Props {}
 
 export const Navigation = (props: Props) => {
   return (
     <BrowserRouter>
-      <nav>
-        <ul className="navBar">
-          <li className="navItem">login</li>
-          <li className="navItem">register</li>
-        </ul>
-      </nav>
-      <div>Páginas</div>
+      <ThemeChanger>
+        <nav>
+          <ul className="navBar">
+            <li className="navItem">login</li>
+            <li className="navItem">register</li>
+          </ul>
+        </nav>
+        <div>Páginas</div>
+      </ThemeChanger>
     </BrowserRouter>
   );
 };
