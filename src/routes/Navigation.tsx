0bf8c763fe2d1b1792672dcd1ded8044b6dacac1
Adrián Antón <adrtler@gmail.com>
2021-12-27 11:07:35 +0100

    Router
    Implementación de router inicial con navbar

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,23 +1,61 @@
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  NavLink,
+} from 'react-router-dom';
 import './navigation.scss';
 import { ThemeContext } from '../context';
 import { useContext } from 'react';
+import { Login, Register } from '../pages';
+import { Home } from '../pages/home/Home';
+
 interface Props {}
 
 export const Navigation = (props: Props) => {
   const { darkMode, changeTheme } = useContext(ThemeContext);
   return (
     <BrowserRouter>
-      <nav>
-        <ul className={darkMode ? 'navBar dark' : 'navBar'}>
-          <li className="navItem">login</li>
-          <li className="navItem">register</li>
+      <nav className={darkMode ? 'navBar dark' : 'navBar'}>
+        <ul className="ulLast">
           <li className="navItem">
             <button onClick={() => changeTheme()}>dark mode</button>
           </li>
+          <li className="navItem">
+            <NavLink
+              to="/register"
+              className={({ isActive }) => (isActive ? 'nav-active' : '')}
+            >
+              Register
+            </NavLink>
+          </li>
+          <li className="navItem">
+            <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? 'nav-active' : '')}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="ulFirst">
+          <NavLink
+            to="/home"
+            className={({ isActive }) => (isActive ? 'nav-active' : '')}
+          >
+            Home
+          </NavLink>
         </ul>
       </nav>
-      <div>Páginas</div>
+      <div style={{ marginLeft: '1rem', marginTop: '10px' }}>
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/*" element={<Navigate to="/home" replace />}></Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
