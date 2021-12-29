edc079712b44b9e27a63dba9355cab95600dc502
Adrián Antón <adrtler@gmail.com>
2021-12-29 13:04:11 +0100

    bootstrap

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,9 @@ interface Props {}
 export const Home = (props: Props) => {
   return (
     <div className="container">
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
       <img
         src="/assets/homepage.jpg"
         alt="Under construction"
