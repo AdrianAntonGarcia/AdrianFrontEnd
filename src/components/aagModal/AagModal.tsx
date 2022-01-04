5693f119ddb91d2240cded2a1e3b2aec6778ce0b
Adrián Antón <adrtler@gmail.com>
2022-01-04 17:57:18 +0100

    Propiedades del modal

--- src/components/aagModal/AagModal.tsx
+++ src/components/aagModal/AagModal.tsx
@@ -1,44 +1,62 @@
 import './aagModal.scss';
 import { AagButton } from '..';
-import { useState } from 'react';
 
 interface Props {
+  onCancel?: () => any;
   onSuccess?: () => any;
+  show: boolean;
+  showCancelButton?: boolean;
+  showSuccessButton?: boolean;
+  textCancel: string;
+  textModal: string;
+  textSuccess: string;
 }
-export const AagModal = ({ onSuccess }: Props) => {
-  const [showModal, setShowModal] = useState(true);
-
-  const cancelModal = () => {
-    setShowModal(false);
-  };
-  if (!showModal) return null;
+export const AagModal = ({
+  onCancel,
+  onSuccess,
+  show = true,
+  showCancelButton = true,
+  showSuccessButton = true,
+  textCancel,
+  textModal,
+  textSuccess,
+}: Props) => {
+  const successButton = (
+    <AagButton
+      label={textSuccess}
+      onClick={() => {
+        !!onSuccess && onSuccess();
+      }}
+    />
+  );
+  const cancelButton = (
+    <AagButton
+      label={textCancel}
+      buttonType="dark"
+      onClick={() => {
+        !!onCancel && onCancel();
+      }}
+    />
+  );
+  if (!show) return null;
   return (
     <div className="modal-container">
       <div className="modal-aag">
         <div className="modal-content-aag">
           <div className="modal-content-container">
-            <span className="modal-text ">
-              Laboris fugiat incididunt elit culpa cupidatat. Anim proident sunt
-              fugiat consectetur ex. Non quis deserunt in ut ullamco occaecat
-              aliqua pariatur dolore consectetur elit cillum. Veniam laborum
-              laboris reprehenderit eu. Nostrud sunt eu velit commodo
-              reprehenderit qui irure eu ea amet sint magna veniam.
-            </span>
-            <div className="modal-buttons">
-              <AagButton
-                label="Confirmar"
-                buttonType="success"
-                onClick={() => {
-                  !!onSuccess && onSuccess();
-                  cancelModal();
-                }}
-              />
-              <AagButton
-                label="Cancelar"
-                buttonType="danger"
-                onClick={cancelModal}
-              />
-            </div>
+            <span className="modal-text ">{textModal}</span>
+            {showCancelButton && showSuccessButton && (
+              <div className="modal-buttons">
+                {showSuccessButton && successButton}
+                {showCancelButton && cancelButton}
+              </div>
+            )}
+            {showCancelButton && !showSuccessButton && (
+              <div className="modal-buttons only">{cancelButton}</div>
+            )}
+            {!showCancelButton && showSuccessButton && (
+              <div className="modal-buttons only">{successButton}</div>
+            )}
           </div>
         </div>
       </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,35 @@
 import { AagModal } from '../../components';
+import { useState, useContext } from 'react';
+import { AagButton } from '../../components/shared/aagButton/AagButton';
+import { ThemeContext } from '../../context';
+import { useTranslation } from 'react-i18next';
 
 interface Props {}
 
 export const Home = (props: Props) => {
-  const onSuccess = (value: string) => {
-    console.log('success');
+  const { darkMode } = useContext(ThemeContext);
+  const [t] = useTranslation();
+  const [showModal, setShowModal] = useState(true);
+  const onSuccess = () => {
+    setShowModal(false);
   };
   return (
-    // <div className="container">
-    //   <button type="button" className="btn btn-primary">
-    //     Primary
-    //   </button>
-    //   <img
-    //     src="/assets/homepage.jpg"
-    //     alt="Under construction"
-    //     style={{ marginTop: '10%', height: '300px', width: '400px' }}
-    //   />
-    // </div>´
-    <AagModal onSuccess={() => onSuccess('adsa')} />
+    <>
+      <AagButton
+        label="Mostrar modal"
+        onClick={() => setShowModal(true)}
+        styleContainer={{ padding: '10px' }}
+      />
+      <AagModal
+        onCancel={() => setShowModal(false)}
+        onSuccess={onSuccess}
+        show={showModal}
+        showCancelButton={true}
+        showSuccessButton={true}
+        textCancel={t('home.Cancel')}
+        textModal="Ad proident deserunt quis duis officia ad. Exercitation excepteur deserunt adipisicing dolor voluptate dolor ullamco officia velit consectetur incididunt dolor. Labore laboris aute laborum consectetur nostrud quis deserunt aute ullamco Lorem magna deserunt officia ea. Sint ipsum consectetur minim commodo. Laboris irure cillum occaecat dolor ad voluptate commodo adipisicing deserunt veniam minim ex. Culpa ipsum exercitation reprehenderit incididunt aute exercitation eu incididunt officia."
+        textSuccess={t('home.Confirm')}
+      />
+    </>
   );
 };
