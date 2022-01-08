88dd99cc70f239d0b940370d5e4fdac9992380ea
Adrián Antón <adrtler@gmail.com>
2022-01-08 22:16:14 +0100

    Avances componente dashboard

--- src/components/aagDashboard/AagDashboard.tsx
+++ src/components/aagDashboard/AagDashboard.tsx
@@ -2,28 +2,46 @@ import styles from './aagDashboard.module.scss';
 import { useState } from 'react';
 import cx from 'classnames';
 import { IoMenuOutline } from 'react-icons/io5';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Cards, Home, List, Login, Register } from '../../pages';
 
 export const AagDashboard = () => {
   const [dashActive, setDashActive] = useState(false);
-  console.log(dashActive);
   return (
     // <div className={styles.container}>
-    <>
+    <BrowserRouter>
       {/* dash */}
-      <div className={cx(styles.dash, dashActive && styles.active)}></div>
-      {/* main */}
-      <div className={cx(styles.main, dashActive && styles.active)}>
-        <div className={cx(styles.btnClose, dashActive && styles.active)}>
-          <div
-            className={cx(styles.btn)}
-            onClick={() => setDashActive(!dashActive)}
-          >
-            <IoMenuOutline />
+      <div className={styles.container}>
+        <div className={cx(styles.dash, dashActive && styles.active)}>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        {/* main */}
+        <div className={cx(styles.main, dashActive && styles.active)}>
+          <div className={cx(styles.btnClose, dashActive && styles.active)}>
+            <div
+              className={cx(styles.btn)}
+              onClick={() => setDashActive(!dashActive)}
+            >
+              <IoMenuOutline />
+            </div>
+          </div>
+          <div className={styles.mainContent}>
+            <Routes>
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/cards" element={<Cards />}></Route>
+              <Route path="/list" element={<List />}></Route>
+              <Route
+                path="/*"
+                element={<Navigate to="/home" replace />}
+              ></Route>
+            </Routes>
           </div>
         </div>
-        <div className={styles.mainContent}>maiin</div>
       </div>
-    </>
-    // </div>
+    </BrowserRouter>
   );
 };
